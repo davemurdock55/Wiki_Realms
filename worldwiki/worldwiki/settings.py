f189ff087fe7494ad8137a142c3e93ea8d07d62f
davemurdock55 <davemurdock55@gmail.com>
2022-12-19 21:17:09 -0700

    fixed the static link in settings.py

--- worldwiki/worldwiki/settings.py
+++ worldwiki/worldwiki/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = "static/"
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'wikiWebsite/static')
+    os.path.join(BASE_DIR, 'worldwiki/static')
 ]
 
 
